27a357d7072533601674caec6e7a1957456f9c9c
Adrian Jones <adrianjones@duck.com>
2023-08-18 12:59:58 -0700

    feat: added some commands
    Added commands from the discord.js tutorial.

---
+++ echo.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const data = new SlashCommandBuilder()
+		.setName('echo')
+		.setDescription('💬 Replies with your input!')
+		.addStringOption(option =>
+			option.setName('input')
+					.setDescription('The input to echo back'))
+					.setRequired(true)
+		.addChannelOption(option => 
+			option.setName('channel')
+					.setDescription('The channel to echo into')
+		.addBooleanOption(option => 
+			option.setName('ephemeral')
+				.setDescription('Whether or not the echo should be ephemeral'));
+
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.cooldowns = new Collection();
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -34,17 +35,36 @@ client.on(Events.InteractionCreate, async interaction => {
 
 	if (!command) return;
 
+	const { cooldowns } = client;
+
+	if (!cooldowns.has(command.data.name)) {
+		cooldowns.set(command.data.name, new Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.data.name);
+	const defaultCooldownDuration = 3;
+	const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+	if (timestamps.has(interaction.user.id)) {
+		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const expiredTimestamp = Math.round(expirationTime / 1000);
+			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+		}
+	}
+
+	timestamps.set(interaction.user.id, now);
+	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
 	try {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
-// Log in to Discord with your client's token
+require('dotenv').config();
 client.login(process.env.TOKEN);
